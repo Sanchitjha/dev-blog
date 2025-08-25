deb03d03b7e4c1de4d210de431b77768417e402c
Sanchitjha <sanchitjha8888@gmail.com>
2025-08-25 18:14:33 +0530

    fix: update API endpoint for fetching posts

--- Frontend/app/page.jsx
+++ Frontend/app/page.jsx
@@ -20,7 +20,7 @@ function HomeContent() {
 
   const getPosts = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/api/posts/");
+      const response = await axios.get("https://dev-blog-pwrn.onrender.com/api/posts/");
       setPosts(response.data);
     } catch (error) {
       console.error("Error fetching posts:", error);
